cd26aabd6c6cb811f16adeaca4f2d6e8cdfa4f41
bhuvanesh shukla <157938005+bhuvaneshshukla1@users.noreply.github.com>
2024-02-02 00:40:41 -0600

    Add files via upload

---
+++ serve.py
@@ -0,0 +1,16 @@
+from flask import Flask,request
+import socket
+import subprocess
+app = Flask(__name__)
+
+@app.route("/")
+def getHostName():
+    return socket.gethostname()
+
+@app.route("/", methods = ['POST'])
+def runSubProcess():
+    result = subprocess.Popen(["python3","/home/ubuntu/stress_cpu.py"])
+    return socket.gethostname()
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0',port=5000)
